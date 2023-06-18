5246578f589878538dfb96d9a364d4127c7a0f8b
Angel <angel.nieva@usach.cl>
2023-06-18 19:27:55 -0400

    proveedor config

---
+++ pep2/proveedor-service/src/main/java/com/mingeso/proveedorservice/ProveedorserviceApplication.java
@@ -0,0 +1,15 @@
+package com.mingeso.proveedorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ProveedorserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProveedorserviceApplication.class, args);
+	}
+
+}
---
+++ pep2/proveedor-service/src/test/java/com/mingeso/proveedorservice/ProveedorserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingeso.proveedorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProveedorserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
